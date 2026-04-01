b06613ff40f832f9675901ed4912d1707e06d573
Holm <you@example.com>
2026-04-01 21:54:04 +0000

    Termine etapa 3

--- client/src/client.c
+++ client/src/client.c
@@ -20,7 +20,7 @@ int main(void)
 	// Escribi: "Hola! Soy un log"
 	log_info(logger, "Hola! Soy un log");
 
-	if (config == NULL){
+	if (logger == NULL){
 		log_error(logger, "¡No se pudo crear el logger!");
 		abort();
 	}
@@ -61,6 +61,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor, conexion);
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
@@ -91,7 +92,7 @@ void leer_consola(t_log* logger)
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
 	while (leido != NULL && strcmp(leido, "") != 0 )
 	{
-		log_info(logger, "text: %s", );
+		log_info(logger, "text: %s", leido);
 
 		free(leido);
 
@@ -106,14 +107,24 @@ void leer_consola(t_log* logger)
 void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
-	char* leido;
-	t_paquete* paquete;
+	char* leido = readline("> ");
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
+	while (leido != NULL && strcmp(leido, "") != 0 )
+	{
+		agregar_a_paquete(paquete, leido, strlen(leido));
 
+		free(leido);
+
+		leido = readline("> ");
+	}
+
+	enviar_paquete(paquete, conexion);
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	free(leido);
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
